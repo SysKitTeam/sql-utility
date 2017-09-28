7f471d009879d644c37cbdabcdb709a3b2a3b4f6
Ivan Cottiero <ivan.cottiero@fer.hr>
2017-09-28 11:42:57 +0200

    Added local project

---
+++ SQLQuickUtilityTool/SQLQuickUtilityTool/ConnectionStringForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SQLQuickUtilityTool
+{
+    public partial class ConnectionStringForm : Form
+    {
+        public string ConnectionString
+        {
+            get
+            {
+                return tbConnectionString.Text;
+            }
+        }
+        public ConnectionStringForm()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }
+
+        private void btnAccept_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+        }
+    }
+}
